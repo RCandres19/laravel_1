b3e05627cfc26a6a6a8003b6a6579d9ae41fdfe1
RCandres <andresconde45678@gmail.com>
2025-03-13 11:12:47 -0500

    update 15

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./resources/**/*.{vue,js,ts,jsx,tsx}"],
-    theme: {
+  content: [
+      './index.html', 
+      './resources/**/*.blade.php', // Asegura que los archivos Blade sean escaneados
+      './resources/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
       extend: {},
-    },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
+
   
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from "@vitejs/plugin-vue";
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
@@ -10,9 +9,16 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue(), //Habilitamos Vue
-        tailwindcss(), //Habilitamos Tailwindcss
+        vue(), // Habilitamos Vue
     ],
+    css: {
+        postcss: {
+            plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+            ],
+        },
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'resources/js'), // Para evitar rutas largas en imports
